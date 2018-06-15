08d00757237edc91b18526d373c4974b219fec38
Jason Andrada <jpa323@nyu.edu>
2018-06-14 23:30:57 -0400

    also add separate bits of state outside of context, and mutators as well

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import Provider from './Provider';
 import Test from './Test';
+import DifferentComponent from './DifferentComponent';
 import './App.css';
 
 class App extends Component {
 
-  logSomethingFromProps() {
+  state = {
+    dark: false,
+    light: false,
+    on: false,
+    off: true
+  }
+
+  constructor(props, context) {
+    super(props)
+    this.changeValues = this.changeValues.bind(this)
+  }
+  logSomethingFromProps() { 
     console.log('you can still log props as normal :)')
   }
+
+  changeValues() {
+    this.setState({
+      dark: !this.state.dark,
+      on: !this.state.on
+    })
+  }
+
+  componentDidUpdate() {
+    console.log('stuff changed', this.state)
+  }
+
   render() {
     return (
       <Provider>
         <Test logSomethingFromProps={this.logSomethingFromProps} />
+        <DifferentComponent changeValues={this.changeValues} />
       </Provider>
     );
   }
---
+++ src/DifferentComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DifferentComponent = props => (
+  <button onClick={props.changeValues}>Change stuff up!</button>
+)
+
+export default DifferentComponent

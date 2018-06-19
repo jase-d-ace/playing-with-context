2dfb9dd27af204a8bddfe0f7d5438ece8ed3418f
Jason Andrada <jpa323@nyu.edu>
2018-06-19 10:59:12 -0400

    add more state play

--- src/NewComponent.js
+++ src/NewComponent.js
@@ -2,6 +2,24 @@ import React, { Component, Fragment } from 'react';
 import { MyContext } from './Provider';
 
 class NewComponent extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      buzz: props.buzz
+    }
+  }
+  componentDidMount() {
+    console.log('reloaded', this.state)
+  }
+
+  renderPropArray() {
+    return this.state.buzz.map( (thing, i) => (<p key={i}>{thing}</p>))
+  }
+
+  componentDidUpdate(prevProps, nextState) {
+    console.log('updated...', prevProps, nextState)
+  }
   render() {
     return (
       <MyContext.Consumer>
@@ -10,6 +28,8 @@ class NewComponent extends Component {
             <p>My brother is named {context.brother} and he is {context.brotherAge} years old. He {context.gamer ? 'is' : 'is not'} a gamer</p>
             <button onClick={context.toggleGamer}>Is he or is he not a gamer?</button>
             <button onClick={this.props.logSomethingFromProps}>Check your console!</button>
+            {this.state.buzz ? this.renderPropArray() : ''}
+            <input onChange={this.props.changeState} type='text' placeholder='put stuff on screen' />
           </Fragment>
         )}
       </MyContext.Consumer>
--- src/Test.js
+++ src/Test.js
@@ -6,9 +6,32 @@ import OtherComponent from './OtherComponent';
 //we imported the createContext() method from our Provider.js file, which splits context into Provider and Consumer. We used Provider to actually have all of our values ready. Consumer is what we use to play with all of that stuff.
 
 class Test extends Component {
+  state = {
+    foo: {
+      bar: 'baz',
+      awk: 0
+    },
+    fizz: true,
+    buzz: ['foo', 'bar']
+  }
+
+  constructor(props) {
+    super(props);
+    this.changeState = this.changeState.bind(this)
+  }
+
   componentDidMount() {
     this.props.logSomethingFromProps();
   }
+
+  changeState(e) {
+    let newState = this.state.buzz;
+    newState.push(e.target.value)
+    this.setState({
+      buzz: newState
+    })
+  }
+
   // The child of any Consumer component is ALWAYS a render prop (aka a function that returns some JSX) instead of pure JSX. The parameter of this render prop is the context that we passed from our Provider "value" prop. In this case, it's the state object that we defined at the very top level of our component tree
   render() {
     return (
@@ -19,7 +42,7 @@ class Test extends Component {
             <button onClick={context.birthday}>Age me one year</button>
             <input type='text' onChange={context.rename} placeholder='give me a new name' name='name' />
             <input type='text' onChange={context.rename} placeholder='give my brother a new name' name='brother' />
-            <NewComponent {...this.props} />
+            <NewComponent {...this.props} buzz={this.state.buzz} changeState={this.changeState} />
             <OtherComponent loadData={context.loadData} />
           </React.Fragment>
         )}
